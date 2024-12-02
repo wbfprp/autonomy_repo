90c1981a2f040d2866c1eba7fa72dc5ba771ea28
aa274 <hittson.finn@gmail.com>
2024-12-02 13:20:32 -0800

    stop sign

--- scripts/frontier_explorer.py
+++ scripts/frontier_explorer.py
@@ -21,6 +21,8 @@ class FrontierExploration(Node):
         self.map_sub = self.create_subscription(OccupancyGrid, "/map", self.map_callback, 10)
         self.state_sub = self.create_subscription(TurtleBotState, "/state", self.state_callback, 10)
         self.nav_success_sub = self.create_subscription(Bool, "/nav_success", self.nav_success_callback, 10)
+        self.detector_sub = self.create_subscription(Bool, "/detector_bool", self.detector_callback, 10)
+        self.hb_timer = self.create_timer(0.1, self.timer_callback)
 
         # Publishers
         self.goal_pub = self.create_publisher(TurtleBotState, "/cmd_nav", 10)
@@ -30,8 +32,13 @@ class FrontierExploration(Node):
         self.state = None
         self.frontiers = []
         self.nav_success = True
-        self.is_planned = False
-        self.plan = None
+        self.goal = TurtleBotState()
+
+        # Stop sign detection
+        self.stop_detected = False
+        self.stop_start_time = None
+        self.ignore_start_time = None
+        self.original_goal = TurtleBotState()
 
     def map_callback(self, msg: OccupancyGrid) -> None:
         # create occupancy map
@@ -53,10 +60,10 @@ class FrontierExploration(Node):
         
         # if self.nav_success:
         self.frontiers = self.explore(self.occupancy)
-        # if not self.is_planned:
-        self.explore_next_frontier()
-        self.get_logger().info('Navigation succeeded. Exploring next frontier.')
-        self.is_planned = False
+        
+        if not self.stop_detected:
+            self.explore_next_frontier()
+            self.get_logger().info('Navigation succeeded. Exploring next frontier.')
 
     def explore(self, occupancy):
         # Copied and pasted from HW4 - P2
@@ -93,16 +100,53 @@ class FrontierExploration(Node):
             distances = np.linalg.norm(self.frontiers - np.array([self.state.x, self.state.y]), axis=1)
 
             next_frontier = self.frontiers[np.argmin(distances)]
-            goal = TurtleBotState()
-            goal.x = next_frontier[0]
-            goal.y = next_frontier[1]
+            
+            self.goal.x = next_frontier[0]
+            self.goal.y = next_frontier[1]
             # goal.theta = np.arctan2(goal.y - self.state.y, goal.x - self.state.x)
-            goal.theta = 0.0
-            self.goal_pub.publish(goal)
-            self.is_planned = True
+            self.goal.theta = 0.0
+            self.goal_pub.publish(self.goal)
         else:
             self.get_logger().info("No frontiers found")
             rclpy.shutdown()
+
+    def detector_callback(self, msg):
+        if msg.data:  # Stop sign true
+            self.get_logger().info('\nStop Sign Detected!\n')
+            current_time = self.get_clock().now().nanoseconds / 1e9
+
+            # check if within the ignore period
+            if self.ignore_start_time and current_time - self.ignore_start_time < 5.1:
+                return
+
+            # Handle stop detection
+            if not self.stop_detected:
+                self.get_logger().info('Stop sign detected. Stopping for 5 sec')
+                self.stop_detected = True
+                self.stop_start_time = current_time
+
+                # # Save the current goal
+                if self.goal:
+                    self.original_goal = self.goal
+
+    def timer_callback(self):
+        current_time = self.get_clock().now().nanoseconds / 1e9
+
+        # Handle stop logic
+        if self.stop_detected:
+            if self.stop_start_time and current_time - self.stop_start_time < 5:
+                # Publish stop command (Navigate to the current state)
+                if self.state:
+                    
+                    self.goal_pub.publish(self.state)
+                    self.get_logger().info('\n\nGoal Pose Written\n\n')
+            else:
+                # End stop period and restore the original goal
+                self.stop_detected = False
+                self.ignore_start_time = current_time
+                self.get_logger().info('Stop duration ended. Resuming navigation.')
+                if self.original_goal:
+                    self.goal_pub.publish(self.original_goal)
 
 
 
